f04e173cb2abdf40f33a2f1079bf16c24c9e6431
NjorogeS <samnjoroge6035@gmail.com>
2021-03-25 11:58:36 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_array_of_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_array_of_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new Application());
+}
+
+class Application extends StatefulWidget {
+  @override
+  _ApplicationState createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+
+  List<int> items = new List();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    for(int i=0;i<50;i++){
+      items.add(i);
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Dummy Application',
+      home: new Scaffold(
+        appBar: new AppBar(title: new Text('List Widget'),),
+        body: new ListView.builder(
+          itemCount: items.length,
+            itemBuilder: (BuildContext context,int index){
+            return new ListTile(
+              title: new Text('item no : $index'),
+              trailing: new Icon(Icons.arrow_forward),
+            );
+            }),
+      ),
+    );
+  }
+}
+
+
+
+
